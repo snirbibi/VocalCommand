65711b93f01cf845925b4e7d694c9d8e8a3d4a6c
snirbibi <45317373+snirbibi@users.noreply.github.com>
2024-10-06 17:25:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const app = express();
 app.use(express.json());
 
 // URL de votre webhook sur Adafruit IO
+const AIO_KEY     = process.env.AIO_KEY;  // Utilisation de la clé d'API depuis les variables d'environnement
 const WEBHOOK_URL = 'https://io.adafruit.com/api/v2/webhooks/feed/6F41S2DuMD2xh93q1oN37xbHioU7/raw'; 
+const AIO_USERNAME= 'snir';
+const TOPIC       = '${AIO_USERNAME}/feeds/alarmeincendie';
+
+// Connexion au serveur MQTT d'Adafruit IO
+const client = mqtt.connect(`mqtts://${AIO_USERNAME}:${AIO_KEY}@io.adafruit.com`);
+
 
 app.post('/webhook', (req, res) => {
   const command = req.body.queryResult.intent.displayName;
@@ -20,6 +27,18 @@ app.post('/webhook', (req, res) => {
   }
 });
 
+// Envoyer des commandes "ON" ou "OFF" via MQTT
+function sendCommandToAdafruitIO(value) {
+  client.publish(TOPIC, value, (error) => {
+    if (error) {
+      console.error('Erreur lors de l\'envoi de la commande:', error);
+    } else {
+      console.log('Commande envoyée avec succès:', value);
+    }
+  });
+}
+
+/*
 async function sendCommandToAdafruitIO(value) {
   try {
     const response = await axios.post(WEBHOOK_URL, value, {
@@ -32,7 +51,7 @@ async function sendCommandToAdafruitIO(value) {
   } catch (error) {
     console.error('Erreur lors de l\'envoi de la commande:', error.response?.data || error.message);
   }
-}
+}*/
 
 // envoi "ON" ou "OFF"
 /*async function sendCommandToAdafruitIO(value) {
