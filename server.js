6f92ed4ff9d59a30d78583e8deab908c03dee24b
snirbibi <45317373+snirbibi@users.noreply.github.com>
2024-10-06 18:17:03 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const AIO_KEY     = process.env.AIO_KEY;   // Utilisation de la clé d'API depuis les variables d'environnement
 //const WEBHOOK_URL = 'https://io.adafruit.com/api/v2/webhooks/feed/6F41S2DuMD2xh93q1oN37xbHioU7/raw'; 
 const AIO_USERNAME= 'snir';
-const TOPIC       = '${AIO_USERNAME}/feeds/alarmeincendie';
+const TOPIC = `${AIO_USERNAME}/feeds/alarmeincendie`; // Utilisez des backticks pour la concaténation
 
 // Connexion au serveur MQTT d'Adafruit IO
 const client = mqtt.connect(`mqtts://${AIO_USERNAME}:${AIO_KEY}@io.adafruit.com`);
@@ -68,7 +68,14 @@ async function sendCommandToAdafruitIO(value) {
     console.error('Erreur lors de l\'envoi de la commande:', error.response?.data || error.message);
   }
 }*/
+// Événements de connexion MQTT
+client.on('connect', () => {
+  console.log('Connecté au serveur MQTT d\'Adafruit IO');
+});
 
+client.on('error', (error) => {
+  console.error('Erreur de connexion au serveur MQTT:', error);
+});
 
 app.listen(3000, () => {
   console.log('Serveur webhook démarré sur le port 3000');
